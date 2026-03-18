42ddc2871b32d086464fc86b17cc9b7687387799
libresoul <contact@damindu.me>
2026-03-18 23:13:40 +0530

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# lifelink
+
+PUSL2023 Group 3 submission
+
+## setting up
+
+```bash
+git clone https://github.com/libresoul/lifelink.git
+cd lifelink
+flutter run -d <device> # or debug with IDE
+```
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('LifeLink')),
+      body: Center(child: Text("LifeLink")),
+    );
+  }
+}

0dee30b902b8f1a5d7e3ccb15534a26b59091733
libresoul <contact@damindu.me>
2026-04-08 19:58:19 +0530

    feat(sign_up_form): obscure password field

--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -14,6 +14,8 @@ class _SignUpFormState extends State<SignUpForm> {
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _isObscured = true;
+  late FocusNode pwFocusNode;
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +38,7 @@ class _SignUpFormState extends State<SignUpForm> {
           ),
           TextFormField(
             controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
               labelText: 'Email',
               border: OutlineInputBorder(),
@@ -47,15 +50,31 @@ class _SignUpFormState extends State<SignUpForm> {
             },
           ),
           TextFormField(
+            focusNode: pwFocusNode,
             controller: _passwordController,
+            obscureText: _isObscured,
+            enableSuggestions: false,
+            autocorrect: false,
             decoration: InputDecoration(
               labelText: 'Password',
               border: OutlineInputBorder(),
+              suffixIcon: pwFocusNode.hasFocus
+                  ? IconButton(
+                      onPressed: () => {
+                        setState(() {
+                          _isObscured = !_isObscured;
+                        }),
+                      },
+                      icon: Icon(
+                        _isObscured ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    )
+                  : null,
             ),
             validator: (value) {
               if (value == null || value.isEmpty) return 'Password is Required';
               if (!isValidPassword(value)) {
-                return 'Password should contain 6 to 20 characters';
+                return 'Password should contain 8 to 20 characters';
               }
               return null;
             },
@@ -79,7 +98,18 @@ class _SignUpFormState extends State<SignUpForm> {
     _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    pwFocusNode.dispose();
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pwFocusNode = FocusNode();
+
+    pwFocusNode.addListener(() {
+      setState(() {});
+    });
   }
 
   bool isValidEmail(String value) {
@@ -93,7 +123,7 @@ class _SignUpFormState extends State<SignUpForm> {
   }
 
   bool isValidPassword(String value) {
-    final passwordRegex = RegExp(r'[\S]{6,20}');
+    final passwordRegex = RegExp(r'[\S]{8,20}');
     return passwordRegex.hasMatch(value);
   }
 
